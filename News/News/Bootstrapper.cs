5393aa612f223dc5f6a4aad6e116b5a513ad1e01
Steve Bilogan <steve.bilogan@gmail.com>
2020-07-16 18:24:44 -0400

    Add skeleton ViewModels and View. Add FontAwesome

---
+++ News/News/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using News.ViewModels;
+using Xamarin.Forms;
+
+namespace News
+{
+	public static class Bootstrapper
+	{
+		public static void Initialize()
+		{
+			var containerBuilder = new ContainerBuilder();
+
+			containerBuilder.RegisterAssemblyTypes(typeof(App).Assembly)
+				.Where(x => x.IsSubclassOf(typeof(ViewModel)));
+
+			var container = containerBuilder.Build();
+			Resolver.Initialize(container);
+		}
+	}
+}
---
+++ News/News/Resolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace News
+{
+	public static class Resolver
+	{
+		private static IContainer Container;
+
+		public static void Initialize(IContainer container)
+		{
+			Container = container;
+		}
+
+		public static T Resolve<T>()
+		{
+			return Container.Resolve<T>();
+		}
+	}
+}
